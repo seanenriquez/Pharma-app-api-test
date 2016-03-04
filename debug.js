5fd672c2143aaba6f4a57f521a01e3c0a02bc00c
menriquez <mrk.enriquez@gmail.com>
2016-03-04 08:42:27 -0500

    dev

--- debug.js
+++ debug.js
@@ -1,3 +1,5 @@
+var test_api_key = "1234567890QWERTYzxcvb";
+
 function showHeaders() {
 	showAuthHeaders();
 	showHeaderHeaders();
@@ -30,57 +32,55 @@ function showParamHeaders() {
 	}
 }
 
-//this specifies the parameter names
-$(".fakeinputname").blur(function() {
-  var newparamname = $(this).val();
-  $(this).parent().parent().parent().parent().find(".realinputvalue").attr("name", newparamname);
-});
- 
-
-$(".close").click(function(e) {
-  e.preventDefault();
-  $(this).parent().remove();
-	showHeaders();
-});
-
-$("#addauthbutton").click(function(e) {
-  e.preventDefault();
-	if ($("#authentication").find(".realinputvalue").length == 0) {
-		$('.httpauth:first').clone(true).appendTo("#authentication");
-	}
-	showHeaders();
-});
 
 $("#addheaderbutton").click(function(e) {
-  e.preventDefault();
+	e.preventDefault();
 	$('.httpparameter:first').clone(true).appendTo("#allheaders");
 	showHeaders();
 });
 
 $("#addprambutton").click(function(e) {
-  e.preventDefault();
+	e.preventDefault();
 	$('.httpparameter:first').clone(true).appendTo("#allparameters");
 	showHeaders();
 });
 
 $("#addfilebutton").click(function(e) {
-  e.preventDefault();
+	e.preventDefault();
 	$('.httpfile:first').clone(true).appendTo("#allparameters");
 	showHeaders();
 });
 
-function postWithAjax(myajax) {
-  myajax = myajax || {};
-  myajax.url = $("#urlvalue").val();
-  myajax.type = $("#httpmethod").val();
-  if (checkForAuth())
-  {
-	  myajax.username = $("#authentication input:first").val();
-	  myajax.password = $("#authentication input").eq(1).val();
-  }
-  myajax.complete = function(jqXHR) {
+//this specifies the parameter names
+$(".fakeinputname").blur(function() {
+	var newparamname = $(this).val();
+	$(this).parent().parent().parent().parent().find(".realinputvalue").attr("name", newparamname);
+});
+
+
+$(".close").click(function(e) {
+	e.preventDefault();
+	$(this).parent().remove();
+	showHeaders();
+});
+
+
+function postWithAjax(myajax,route,type) {
+
+	myajax = myajax || {};
+	myajax.url = $("#urlvalue").val() + route;
+	myajax.type = type;
+
+	if (checkForAuth())
+	{
+		myajax.username = $("#authentication input:first").val();
+		myajax.password = $("#authentication input").eq(1).val();
+	}
+
+	myajax.complete = function(jqXHR) {       
+
 		$("#statuspre").text(
-				"HTTP " + jqXHR.status + " " + jqXHR.statusText);
+			"HTTP " + jqXHR.status + " " + jqXHR.statusText);
 		if (jqXHR.status == 0) {
 			httpZeroError();
 		} else if (jqXHR.status >= 200 && jqXHR.status < 300) {
@@ -108,28 +108,33 @@ function postWithAjax(myajax) {
 	$("#statuspre").removeClass("alert-error");
 	$("#statuspre").removeClass("alert-warning");
 
-  $('#ajaxspinner').show();
+	$('#ajaxspinner').show();
 	var req = $.ajax(myajax).always(function(){
-    $('#ajaxspinner').hide();
+		$('#ajaxspinner').hide();
 	});
 }
 
 $("#submitajax").click(function(e) {
-  e.preventDefault();
-  if(checkForFiles()){
-    postWithAjax({
-      headers: createHeaderData(),
-      data : createMultipart(), 
-      cache: false,
-      contentType: false,
-      processData: false  
-    });
-  } else {
-    postWithAjax({
-      headers : createHeaderData(),
-      data : createUrlData()
-    });    
-  }
+
+	e.preventDefault();
+	if(checkForFiles()){
+		postWithAjax({
+			headers: createHeaderData(),
+			data : createMultipart(), 
+			cache: false,
+			contentType: false,
+			processData: false,  
+			xhrFields: { withCredentials: true },
+			crossDomain: true
+		});
+	} else {
+		postWithAjax({
+			headers : createHeaderData(),
+			data : createUrlData(),
+			xhrFields: { withCredentials: true },
+			crossDomain: true
+		});    
+	}
 });
 
 function checkForFiles() {
@@ -141,7 +146,8 @@ function checkForAuth() {
 }
 
 function createUrlData(){
-  var mydata = {};
+
+	var mydata = {};
 	var parameters = $("#allparameters").find(".realinputvalue");
 	for (i = 0; i < parameters.length; i++) {
 		name = $(parameters).eq(i).attr("name");
@@ -151,31 +157,31 @@ function createUrlData(){
 		value = $(parameters).eq(i).val();
 		mydata[name] = value
 	}
-  return(mydata);
+	return(mydata);
 }
 
 function createMultipart(){
-  //create multipart object
-  var data = new FormData();
-  
-  //add parameters
-  var parameters = $("#allparameters").find(".realinputvalue");
+	//create multipart object
+	var data = new FormData();
+
+	//add parameters
+	var parameters = $("#allparameters").find(".realinputvalue");
 	for (i = 0; i < parameters.length; i++) {
 		name = $(parameters).eq(i).attr("name");
 		if (name == undefined || name == "undefined") {
 			continue;
 		}
-    if(parameters[i].files){
-  	  data.append(name, parameters[i].files[0]);      
-    } else {
-		  data.append(name, $(parameters).eq(i).val());
-    }
+		if(parameters[i].files){
+			data.append(name, parameters[i].files[0]);      
+		} else {
+			data.append(name, $(parameters).eq(i).val());
+		}
 	}
-  return(data)  
+	return(data)  
 }
 
 function createHeaderData(){
-  var mydata = {};
+	var mydata = {};
 	var parameters = $("#allheaders").find(".realinputvalue");
 	for (i = 0; i < parameters.length; i++) {
 		name = $(parameters).eq(i).attr("name");
@@ -185,9 +191,59 @@ function createHeaderData(){
 		value = $(parameters).eq(i).val();
 		mydata[name] = value
 	}
-  return(mydata);
+	return(mydata);
 }
 
 function httpZeroError() {
 	$("#errordiv").append('<div class="alert alert-error"> <a class="close" data-dismiss="alert">&times;</a> <strong>Oh no!</strong> Javascript returned an HTTP 0 error. One common reason this might happen is that you requested a cross-domain resource from a server that did not include the appropriate CORS headers in the response. Better open up your Firebug...</div>');
 }
+
+$("#testaboutbutton").click(function(e) {
+	
+	e.preventDefault();
+	
+	var base_api_uri = $("#urlvalue").val();
+
+	$("#outputframe").hide();
+	$("#outputpre").empty();
+	$("#headerpre").empty();
+	$("#outputframe").attr("src", "")
+	$("#ajaxoutput").show();
+	$("#statuspre").text("0");
+	$("#statuspre").removeClass("alert-success");
+	$("#statuspre").removeClass("alert-error");
+	$("#statuspre").removeClass("alert-warning");
+	$('#ajaxspinner').show();
+	
+	$.ajax({
+		url:  base_api_uri+"api/about/"+test_api_key,
+		dataType: 'json',
+		xhrFields: {
+			withCredentials: true
+		},
+		crossDomain: true,
+		success: function (data, status) {
+			$('#ajaxspinner').hide();
+			
+			$("#outputpre").append(JSON.stringify(data));
+			
+			$.each(data, function (key, value) {
+				//handle the data  
+				console.log(key,value);
+			});
+
+
+		},
+		error: function (ts) {
+			$('#ajaxspinner').hide();
+			$("#errordiv").append(ts.responseText);
+		},
+		
+		complete: function () {
+        // Handle the complete event
+        alert("ajax completed " + cartObject.productID);
+      }
+	});
+
+});
+
